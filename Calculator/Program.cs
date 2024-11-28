dbe9c6a0f1c170af18294a5abc39948886ee8e44
JhonnyGames <borozdinaes89@gmail.com>
2024-11-28 20:44:23 +0500

    pr1+

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,7 +7,23 @@ namespace SimpleCalculator
         {
             ICalculator calculator = new Calculator();
 
+            try
+            {
+                Console.WriteLine("Enter first number");
+                float value1 = float.Parse(Console.ReadLine());
 
+                Console.WriteLine("Enter first number");
+                float value2 = float.Parse(Console.ReadLine());
+
+                float result = calculator.Addition(value1, value2);
+
+                Console.WriteLine($"Sum of numbers {value1} and {value2} = {result}");
+            }
+
+            catch(Exception e)
+            {
+                Console.Write(e.ToString());
+            }
             
         }
     }
