d2cee8028f8f64c4ea59e388505ac766c162ceb4
JhonnyGames <borozdinaes89@gmail.com>
2024-11-28 20:59:34 +0500

    interface+class

---
+++ Calc_DependencyInjection/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc_DependencyInjection
+{
+    internal class Logger : ILogger
+    {
+        void ILogger.Error()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILogger.Event()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
