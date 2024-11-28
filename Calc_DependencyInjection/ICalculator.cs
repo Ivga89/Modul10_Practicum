a5d6a4fcc1aa88a66f2d86d6ee63b127d587bc7f
JhonnyGames <borozdinaes89@gmail.com>
2024-11-28 20:50:05 +0500

    pr2 initial

---
+++ Calc_DependencyInjection/ICalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DICalculator
+{
+    public interface ICalculator
+    {
+        float Addition(float value1, float value2);
+    }
+}
---
+++ Calc_DependencyInjection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DICalculator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
